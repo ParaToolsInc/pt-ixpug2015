bd426320777f058c5e6e83af78fca58c4f7d79e6
Srinath Vadlamani <srinath@txcorp.com>
2015-09-28 14:55:21 -0700

    adding examples

---
+++ host-vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PATH=/project/projectdirs/acts/vadlaman/tau/2.24.1/intel/16/x86_64/bin:$PATH
+
+module unload papi
+module load papi/host-5.3.0
+module load java
+
+export TAU_MAKEFILE=/project/projectdirs/acts/vadlaman/tau/2.24.1/intel/16/x86_64/lib/Makefile.tau-icpc-papi-ompt-mpi-pdt-openmp
---
+++ mic-vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export PATH=/project/projectdirs/acts/vadlaman/tau/2.24.1/intel/16/mic_linux/bin:$PATH
+
+module unload papi
+module load papi/mic-5.3.0
+
+source /project/projectdirs/acts/vadlaman/perl-mic/perlvars.sh
+
+export TAU_MAKEFILE=/project/projectdirs/acts/vadlaman/tau/2.24.1/intel/16/mic_linux/lib/Makefile.tau-icpc-papi-ompt-mpi-pdt-openmp
