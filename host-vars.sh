45b81aa2831c2a9b3f5243bf6f60ef484ce3eb89
Srinath Vadlamani <srinath@txcorp.com>
2015-09-29 10:23:32 -0700

    removed ptts bin because of view conflict

--- host-vars.sh
+++ host-vars.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export PATH=/project/projectdirs/acts/vadlaman/tau/2.24.1/intel/16/x86_64/bin:$PATH
-export PATH=/project/projectdirs/acts/vadlaman/ptts/1.2.2/bin:$PATH
 module unload papi
 module load papi/host-5.3.0
 module load java
